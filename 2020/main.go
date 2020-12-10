b36177329e689c39a4bf75622bfeffc9921f5fd0
Lars Delhage <lars.delhage@konsult.atg.se>
2020-12-11 00:01:27 +0100

    Cleanup main.go

--- 2020/main.go
+++ 2020/main.go
@@ -10,15 +10,7 @@ import (
 )
 
 func main() {
-	//fmt.Printf("%d number of bag colors contain at least one 'shiny gold'\n", day07.Solve1())
-	//fmt.Printf("%d bags are required inside one 'shiny gold' bag\n", day07.Solve2())
-	//acc,loop := day08.Solve1()
-	//fmt.Printf("Accumulator is %d. Loop found: %t\n", acc, loop)
-	//acc = day08.Solve2()
-	//fmt.Printf("Accumulator is %d", acc)
-	//fmt.Println(day09.Solve1())
-	//fmt.Println(day09.Solve2())
-	//fmt.Println(day10.Solve1())
+
 	switch os.Args[1] {
 	case "day07":
 		utils.Perf("2020-12-07", day07.Solve1, day07.Solve2)
@@ -27,7 +19,4 @@ func main() {
 	case "day10":
 		utils.Perf("2020-12-10", day10.Solve1, day10.Solve2)
 	}
-	//start := time.Now()
-	//fmt.Println(day10.Solve2())
-	//fmt.Printf("Time spent: %s\n", time.Since(start))
 }
