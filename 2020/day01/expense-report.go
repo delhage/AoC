a14fbd960aad69ec85acf6f048da0a0f267c9199
Lars Delhage <lars.delhage@konsult.atg.se>
2020-12-13 23:11:34 +0100

    Refactored day01

--- 2020/day01/expense-report.go
+++ 2020/day01/expense-report.go
@@ -1,39 +1,51 @@
-package main
+package day01
 
 import (
-	"bufio"
-	"fmt"
-	"os"
 	"strconv"
+
+	"../utils"
 )
 
-// ReadExpense reads a file into a slice of int:s
-func ReadExpense(path string) ([]int, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
+var input, _ = utils.ReadFile("day01/input.txt")
+
+// ParseLines reads a file into a slice of int:s
+func ParseLines(input []string) []int {
+	var expense []int
+	for _, s := range input {
+		i, _ := strconv.Atoi(s)
+		expense = append(expense, i)
 	}
-	defer file.Close()
-	var lines []int
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		i, _ := strconv.Atoi(scanner.Text())
-		lines = append(lines, i)
-	}
-	return lines, scanner.Err()
+	return expense
 }
 
-func main() {
+// Solve1 returns answer to first problem
+func Solve1() int {
 	var res int
-	x, _ := ReadExpense(os.Args[1])
+	x := ParseLines(input)
 	for i, s := range x {
 		for j := i + 1; j < len(x); j++ {
 			res = s + x[j]
-			// fmt.Println(res)
 			if res == 2020 {
-				fmt.Println(s * x[j])
-				os.Exit(0)
+				return s * x[j]
 			}
 		}
 	}
+	return -1
+}
+
+// Solve2 returns answer to second problem
+func Solve2() int {
+	var res int
+	x := ParseLines(input)
+	for i, s := range x {
+		for j := i + 1; j < len(x); j++ {
+			for k := j + i; k < len(x); k++ {
+				res = s + x[j] + x[k]
+				if res == 2020 {
+					return s * x[j] * x[k]
+				}
+			}
+		}
+	}
+	return -1
 }
