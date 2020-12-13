117b934128c7770f33f984748c06188c4f6d7ed8
Lars Delhage <lars.delhage@konsult.atg.se>
2020-12-13 23:12:15 +0100

    Refactored day01

--- 2020/main.go
+++ 2020/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"./day01"
 	"./day07"
 	"./day09"
 	"./day10"
@@ -15,6 +16,8 @@ import (
 func main() {
 
 	switch os.Args[1] {
+	case "day01":
+		utils.Perf("2020-12-01", day01.Solve1, day01.Solve2)
 	case "day07":
 		utils.Perf("2020-12-07", day07.Solve1, day07.Solve2)
 	case "day09":
