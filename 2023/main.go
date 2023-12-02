0a21654ff41371292dc64c888d4bf36a64e6c03f
Lars Delhage <lasse@nohup.se>
2023-12-02 15:47:17 +0100

    Finished day2

--- 2023/main.go
+++ 2023/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"aoc/2023/day01"
+	"aoc/2023/day02"
 	"aoc/2023/utils"
 )
 
@@ -12,5 +13,7 @@ func main() {
 	switch os.Args[1] {
 	case "day01":
 		utils.Perf("2023-12-01", day01.Solve1, day01.Solve2)
+	case "day02":
+		utils.Perf("2023-12-02", day02.Solve1, day02.Solve2)
 	}
 }
