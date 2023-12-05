b89e55178feaba3456ebb0fad1833d48ba522174
Lars Delhage <lasse@nohup.se>
2023-12-05 22:51:36 +0100

    Add day03

--- 2023/main.go
+++ 2023/main.go
@@ -5,6 +5,7 @@ import (
 
 	"aoc/2023/day01"
 	"aoc/2023/day02"
+	"aoc/2023/day03"
 	"aoc/2023/utils"
 )
 
@@ -15,5 +16,7 @@ func main() {
 		utils.Perf("2023-12-01", day01.Solve1, day01.Solve2)
 	case "day02":
 		utils.Perf("2023-12-02", day02.Solve1, day02.Solve2)
+	case "day03":
+		utils.Perf("2023-12-03", day03.Solve1, day03.Solve2)
 	}
 }
