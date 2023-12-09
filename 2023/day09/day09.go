5e75738dee74428be97ad2db0b4b3f7291e6c8c7
Lars Delhage <lasse@nohup.se>
2023-12-09 18:07:14 +0100

    Day 9. Initial commit

---
+++ 2023/day09/day09.go
@@ -0,0 +1,17 @@
+package day09
+
+import (
+	"aoc/2023/utils"
+)
+
+var input, _ = utils.ReadFile("day04/input.txt")
+
+// Solve1 returns answer to first problem
+func Solve1() int {
+	return 1
+}
+
+// Solve2 returns answer to second problem
+func Solve2() int {
+	return 1
+}
